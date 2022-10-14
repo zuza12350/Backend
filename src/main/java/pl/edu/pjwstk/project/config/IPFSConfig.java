e3f4d645321632a913cd5fcf32145d41fa32edb4
ZuZa1235012350 <s21243@pjwstk.edu.pl>
2022-10-14 10:14:16 +0200

    Fixing bug

--- src/main/java/pl/edu/pjwstk/project/config/IPFSConfig.java
+++ src/main/java/pl/edu/pjwstk/project/config/IPFSConfig.java
@@ -1,6 +1,7 @@
 package pl.edu.pjwstk.project.config;
 
 import io.ipfs.api.IPFS;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -9,9 +10,14 @@ import org.springframework.context.annotation.Scope;
 @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class IPFSConfig {
     IPFS ipfs;
+    @SneakyThrows
     public IPFSConfig() {
+//        ipfs = new IPFS("/dnsaddr/ipfs.infura.io/tcp/5001/https");
+//        System.out.println("connected");
+//        //System.out.println("id: " + ipfs.id());
         ipfs = new IPFS("localhost", 5001);
     }
+
 
     public IPFS getIpfs() {
         return ipfs;
