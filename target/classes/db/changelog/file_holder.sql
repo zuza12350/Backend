8a4530cba722ffea0802ea082f59cae2b7ef649f
Mikolaj <nogamikolaj00@gmail.com>
2022-10-20 19:34:17 +0200

    remove unwanted files and add gitignore

--- target/classes/db/changelog/file_holder.sql
+++
@@ -1,7 +0,0 @@
-CREATE SEQUENCE IF NOT EXISTS hibernate_sequence;
-
-CREATE TABLE file_holder(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    file_name VARCHAR NOT NULL,
-    file_hash VARCHAR NOT NULL
-);
