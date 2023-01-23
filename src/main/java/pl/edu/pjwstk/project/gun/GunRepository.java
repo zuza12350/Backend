3e33a00aa083950419a3799b18f7071de73a6377
ZuZa1235012350 <s21243@pjwstk.edu.pl>
2023-01-23 21:00:16 +0100

    Adding method to gun service

--- src/main/java/pl/edu/pjwstk/project/gun/GunRepository.java
+++ src/main/java/pl/edu/pjwstk/project/gun/GunRepository.java
@@ -1,6 +1,7 @@
 package pl.edu.pjwstk.project.gun;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.sun.istack.Nullable;
 import org.springframework.stereotype.Repository;
 import pl.edu.pjwstk.project.gun.requests.GunRequest;
@@ -14,6 +15,7 @@ import pl.edu.pjwstk.project.gun.requests.GunTypeRequest;
 @Repository
 public interface GunRepository {
     void setGunsFromFile();
+    JsonObject getWholeGunData();
     JsonArray getGunData(String kind);
     void addGun(Long categoryId, @Nullable Long subType, GunRequest gunRequest);
     void addGunType(GunTypeRequest gunTypeRequest);
